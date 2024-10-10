26ba62a07f9df3e1043d0a662f30942ae88aa86f
koshitake2m2 <blueling2m2dvp@gmail.com>
2024-10-10 23:54:15 +0900

    add

---
+++ raycast_scripts/calendar.zsh
@@ -0,0 +1,12 @@
+ #!/bin/zsh
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open Calendar in Chrome
+# @raycast.mode compact
+#
+# Optional parameters:
+# @raycast.icon 📅
+ 
+ open -na 'Google Chrome' --args --profile-directory="Profile 1" 'https://calendar.google.com/calendar/u/0/r/week'
+
